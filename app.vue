7cb687ab3dd7bb7f0db77c99cb2eaed539d16084
Paweł Płonka <pjplonka@gmail.com>
2024-05-20 23:34:41 +0200

    Initial commit - Nuxt 3, Vuetify 3, Base layout.

---
+++ app.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+
+    <v-layout class="rounded rounded-md">
+
+      <v-app-bar title="Application bar"></v-app-bar>
+
+      <v-navigation-drawer permanent>
+        <v-list>
+          <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
+          <v-divider></v-divider>
+          <v-list-item link title="List Item 1"></v-list-item>
+          <v-list-item link title="List Item 2"></v-list-item>
+          <v-list-item link title="List Item 3"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+        Main Content
+      </v-main>
+    </v-layout>
+
+  </v-app>
+</template>
+<script setup lang="ts">
+</script>
